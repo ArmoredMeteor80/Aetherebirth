edbb9bc9de0615d412c9f398e522ff228dfecdb1
ArmoredMeteor80 <armoredmeteor80@gmail.com>
2022-12-18 22:19:56 +0100

    Taux de rafraîchissement de la fenêtre à 60 ips

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pygame
 pygame.init()
 
 # Création de la fenêtre de jeu
-pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((1280, 720))
 # Change le titre de la fenêtre
 pygame.display.set_caption("Pyb0b")
+# Création d'un objet "Clock" permettant de gérer le temps
+clock = pygame.time.Clock()
 
 # Boucle du jeu
 running = True
@@ -16,6 +18,8 @@ while running:
         # Stoppe la boucle de jeu lorsque la fenêtre est fermée
         if event.type == pygame.QUIT:
             running = False
+    # Cadence le taux de rafraîchissement de la fenêtre à 60 ips
+    clock.tick(60)
 
 # Déinitialise tous les modules pygame
 pygame.quit()
