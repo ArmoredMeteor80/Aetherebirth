ecb5bb37b12f96eb118230c3160da6bf45c12ad7
ArmoredMeteor80 <armoredmeteor80@gmail.com>
2022-12-19 15:53:21 +0100

    Réorganisation en deux fichiers, ajout classe Game

---
+++ game.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+class Game:
+    """Représentation du concept du jeu"""
+    def __init__(self):
+        """Constructeur"""
+        # Création de la fenêtre de jeu
+        pygame.display.set_mode((1280, 720))
+        # Change le titre de la fenêtre
+        pygame.display.set_caption("Pyb0b")
+
+    def run(self):
+        """Boucle du jeu"""
+        # Création d'un objet "Clock" permettant de gérer le temps en tant
+        clock = pygame.time.Clock()
+        running = True
+
+        while running:
+
+            for event in pygame.event.get():
+                # Stoppe la boucle de jeu lorsque la fenêtre est fermée
+                if event.type == pygame.QUIT:
+                    running = False
+
+            # Cadence le taux de rafraîchissement de la fenêtre à 60 ips
+            clock.tick(60)
+
+        # Déinitialise tous les modules pygame
+        pygame.quit()
--- main.py
+++ main.py
@@ -1,25 +1,10 @@
 import pygame
 
-# Initialise tous les modules Pygame
-pygame.init()
+from game import Game
 
-# Création de la fenêtre de jeu
-screen = pygame.display.set_mode((1280, 720))
-# Change le titre de la fenêtre
-pygame.display.set_caption("Pyb0b")
-# Création d'un objet "Clock" permettant de gérer le temps
-clock = pygame.time.Clock()
-
-# Boucle du jeu
-running = True
-
-while running:
-    for event in pygame.event.get():
-        # Stoppe la boucle de jeu lorsque la fenêtre est fermée
-        if event.type == pygame.QUIT:
-            running = False
-    # Cadence le taux de rafraîchissement de la fenêtre à 60 ips
-    clock.tick(60)
-
-# Déinitialise tous les modules pygame
-pygame.quit()
+if __name__ == '__main__':
+    # Initialise tous les modules Pygame
+    pygame.init()
+    # Création d'un objet "Game" gérant le jeu
+    game = Game()
+    game.run()
